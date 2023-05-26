bf6dd7c4b39eb2f58fb81b6fd467fafdb2ee1ae6
DanielFlores1223 <dany-232000@hotmail.com>
2023-05-26 16:23:24 -0600

    Course started

---
+++ base/app.js
@@ -0,0 +1,9 @@
+"use strict";
+const message = 'Hello world!!!';
+const hero = {
+    name: 'Ironman',
+    age: 45
+};
+hero.age = 50;
+console.log(message);
+console.log(hero.age);
---
+++ base/app.ts
@@ -0,0 +1,11 @@
+const message = 'Hello world!!!';
+
+const hero = {
+    name: 'Ironman',
+    age: 45
+}
+
+hero.age = 50;
+
+console.log(message);
+console.log(hero.age);
