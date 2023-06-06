26aad1b91c6725017ca16e83609a9ecb9be5bba8
DanielFlores1223 <dany-232000@hotmail.com>
2023-06-05 22:35:30 -0600

    extends typescript, setters and getters

--- classes/extends.ts
+++ classes/extends.ts
@@ -1,12 +1,41 @@
 (() => {
-    class Avenger {
-        constructor(
-            public name: string,
-            public realName: string,
-            
-        ){
-            console.log('Constructor called')
-        }
+  class Avenger {
+    constructor(public name: string, public realName: string) {
+      console.log("Constructor Avenger called");
     }
 
+    // protected access, you can access to methods from extends
+    // private access, you can acces to methods inside class
+    protected getFullName() {
+      return `${this.name} ${this.realName}`;
+    }
+  }
+
+  class Xmen extends Avenger {
+    constructor(name: string, realName: string, public isMutant: boolean) {
+      super(name, realName);
+      console.log("Xmen Constructor called");
+    }
+
+    get fullName(): string {
+      return `${this.name} - ${this.realName}`;
+    }
+
+    set fullName(name: string) {
+      if (this.name.length < 3)
+        throw new Error("name property must have 3 characteres or more");
+      this.name = name;
+    }
+
+    getFullNameFromXmen() {
+      super.getFullName();
+    }
+  }
+
+  // if the Xmen constructor does not exist in Xmen class, typescript cretes it automatically taking Avenger constructor
+
+  const wolverine = new Xmen("Wolverine", "Logan", true);
+
+  console.log(wolverine.fullName); //get, you dont need to execute it, it works well
+  wolverine.fullName = "Pedri"; // set new values
 })();
--- main.js
+++ main.js
@@ -21,8 +21,32 @@
         constructor(name, realName) {
             this.name = name;
             this.realName = realName;
-            console.log('Constructor called');
+            console.log("Constructor Avenger called");
+        }
+        getFullName() {
+            return `${this.name} ${this.realName}`;
         }
     }
+    class Xmen extends Avenger {
+        constructor(name, realName, isMutant) {
+            super(name, realName);
+            this.isMutant = isMutant;
+            console.log("Xmen Constructor called");
+        }
+        get fullName() {
+            return `${this.name} - ${this.realName}`;
+        }
+        set fullName(name) {
+            if (this.name.length < 3)
+                throw new Error("name property must have 3 characteres or more");
+            this.name = name;
+        }
+        getFullNameFromXmen() {
+            super.getFullName();
+        }
+    }
+    const wolverine = new Xmen("Wolverine", "Logan", true);
+    console.log(wolverine.fullName);
+    wolverine.fullName = "Pedri";
 })();
 //# sourceMappingURL=main.js.map
